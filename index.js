4c11b281cc62074c6df5345116c2b507bd0914ed
SteveGuillermoAC <45379600+SteveGuillermoAC@users.noreply.github.com>
2020-02-20 13:01:08 -0500

    interfaz chat

---
+++ index.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+//Consiguracion
+app.set('port', process.env. PORT || 3000);
+
+//Archivos estaticos
+app.use(express.static(path.join(__dirname,'public')));
+
+//Empezar el servidor
+const server =app.listen(app.get('port'), ()=>{
+    console.log('Server on port', app.get('port'));
+});
+
+//webSocket
+const SocketIO =require('socket.io');
+const io = SocketIO(server);
+
+io.on('connection', (socket)=>{
+    console.log('new connection', socket.id);
+
+    socket.on('chat:message' , (data)=>{
+
+        io.sockets.emit('chat:message', data)
+ })  
+});
+
+
+
+
